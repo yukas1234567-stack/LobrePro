3b8cb2364d0c6418b0ae7277f56f716cb9653dbe
yukas1234567-stack <2.81434972e+08+yukas1234567-stack@users.noreply.github.com>
2026-06-03 07:29:54 +0000

    Remove boot animation and add PWA update mechanism

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lobre-pro-v1';
+const CACHE_NAME = 'lobre-pro-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -9,6 +9,7 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
